7d9b05f6e2e8abaf757c494cf4ae553db7bc8069
Adam Workman <adammworkman25@gmail.com>
2020-06-17 18:59:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,14 +8,13 @@ def line(people_in_line)
     arr = []
    people_in_line.each_with_index do |name, index|
      arr.push ("#{index + 1}. #{name}")
-     
-   end
+     end
    puts "The line is currently: #{arr.join(" ")}"
   end
 end
 
-def take_a_number(katz_deli, name)
-puts "Welcome, #{name}. You are number #{katz_deli.length + 1} in line."
+def take_a_number(number, name)
+puts "Welcome, #{name}. You are number #{number.length + 1} in line."
 end
 
 def now_serving(array)
